7385d8020552a68568b61459c75faa453db0baf7
Mylab <labs@onmycrowd.com>
2020-05-04 20:44:51 -0700

    Adding some ui to the landing page

--- src/views/landing/shell.cljs
+++ src/views/landing/shell.cljs
@@ -8,12 +8,24 @@
      [:div.level-item.has-text-centered
       [:div
        [:p "Let's start from scratch!"]
-       [:p "We offer you one year Free Tier that includes all the software you need for your business custom made"]
-       [:p "What's in it for us? A long term relationship with our clients and a year from now we talk money"]]]]
-    [:div.level
-     [:div.level-item.has-text-centered
-      [:div.field.has-addons
-       [:p.control
-        [:input.input {:type "text" :placeholder "Email"}]]
-       [:p.control
-        [:button.button "Done!"]]]]]]])
+       [:p "I offer you one year Free Tier that includes all the software you need for your business custom made"]
+       [:p "What's in it for me? A long term relationship with my clients and a year from now we talk money"]]]]]
+   [:div.level
+    [:div.level-item.has-text-centered
+     [:p "If you decide to get in touch, find me in SKYPE as "
+      [:strong.heading "Busqandote"]]]]
+   [:div.hero-body
+   [:div.level
+    [:div.level-item.has-text-centered
+     [:div
+      [:p.heading "prospects"]
+      [:p.title "0"]]]
+    [:div.level-item.has-text-centered
+     [:div
+      [:p.heading "clients"]
+      [:p.title "0"]]]
+    [:div.level-item.has-text-centered
+     [:div
+      [:p.heading "staff"]
+      [:p.title "1"]]]]]
+   ])
