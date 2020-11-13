4ec25b83b8aa9642361e3cd3e7198c6fc943985f
Mylab <labs@onmycrowd.com>
2020-11-13 00:10:04 -0700

    Refactor to use a json file.

--- terraform/static.tf
+++ terraform/static.tf
@@ -1,24 +1,7 @@
 resource "aws_s3_bucket" "b" {
   bucket = "busqandote.com"
   acl    = "public-read"
-  policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "PublicReadGetObject",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-          "s3:GetObject"
-        ],
-        "Resource": [
-          "arn:aws:s3:::busqandote.com/*"
-        ]
-      }
-    ]
-  }
-  POLICY
+  policy = file("terraform/policy.json")
 
   website {
     index_document = "index.html"
