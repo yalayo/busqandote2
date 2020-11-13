ee0cd51b76c7d25aacb8a21cee8605a7c8acb289
Mylab <labs@onmycrowd.com>
2020-11-12 23:51:41 -0700

    Adding policy to allow public access to the bucket objects.

--- terraform/static.tf
+++ terraform/static.tf
@@ -1,7 +1,24 @@
 resource "aws_s3_bucket" "b" {
   bucket = "busqandote.com"
   acl    = "public-read"
-  #policy = file("policy.json")
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadGetObject",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Resource": [
+          "arn:aws:s3:::busqandote.com/*"
+        ]
+      }
+    ],
+  }
+  POLICY
 
   website {
     index_document = "index.html"
